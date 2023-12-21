306814b3dc284bf0052e3bfd9f0bb0116da8640b
Hashem <143422149+hshemdev@users.noreply.github.com>
2023-12-22 02:11:25 +0530

    Add files via upload

---
+++ ConverseAI.py
@@ -0,0 +1,19 @@
+import openai
+import gradio 
+
+openai.api_key = "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"
+
+messages = [{"role": "system", "content": "You are a financial experts that specializes in real estate investment and negotiation"}]
+
+def CustomChatGPT(user_input_exceeded_API_Quota):
+    messages.append({"role": "user", "content": user_input_exceeded_API_Quota})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "AI chatbot")
+
+demo.launch(share=True) 
